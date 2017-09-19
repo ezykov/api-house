e7316e1af7cdd38cd55ea5d6d05615713db108e0
Evgeny Zykov <mail@ezykov.com>
2017-09-20 01:24:45 +0300

    First commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'V3.1 to deploy Express' });
+});
+
+module.exports = router;
